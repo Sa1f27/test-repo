d4056cb612e59bc078f4c08bad270fea0b8953f9
mohankumarpolimera <mohankumarpolimera@gmail.com>
2025-04-03 14:54:52 +0530

    i have added some changes in main

---
+++ main.py
@@ -0,0 +1,12 @@
+# Asking for user input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+profession = input("Enter your profession: ")
+
+
+# Printing the introduction
+print("\n--- User Introduction ---")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Profession: {profession}")
+
